2ddc8770c54c8ab57216d8fdcca289941171ff67
StevePro7 <steveprozune@hotmail.com>
2021-04-03 18:58:05 +0100

    no message

--- dev/banks/bank2.h
+++ dev/banks/bank2.h
@@ -1,15 +1,3 @@
-extern const unsigned char	main_tiles__palette__bin[];
-#define				main_tiles__palette__bin_size 16
-#define				main_tiles__palette__bin_bank 2
-
-extern const unsigned char	main_tiles__tilemap__bin[];
-#define				main_tiles__tilemap__bin_size 312
-#define				main_tiles__tilemap__bin_bank 2
-
-extern const unsigned char	main_tiles__tiles__psgcompr[];
-#define				main_tiles__tiles__psgcompr_size 2238
-#define				main_tiles__tiles__psgcompr_bank 2
-
 extern const unsigned char	splash__palette__bin[];
 #define				splash__palette__bin_size 16
 #define				splash__palette__bin_bank 2
