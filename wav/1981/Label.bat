da988cc458f733b2712024ec8cd9a4dd0dae55e7
StevePro7 <steveprozune@hotmail.com>
2021-04-04 21:31:12 +0100

    no message

---
+++ wav/1981/Label.bat
@@ -0,0 +1,5 @@
+pcmenc -rto 1 -dt1 12 -dt2 12 -dt3 423 Riff_1981_01.wav
+pcmenc -rto 1 -dt1 12 -dt2 12 -dt3 423 Riff_1981_02.wav
+pcmenc -rto 1 -dt1 12 -dt2 12 -dt3 423 Riff_1981_03.wav
+pcmenc -rto 1 -dt1 12 -dt2 12 -dt3 423 Riff_1981_04.wav
+pcmenc -rto 1 -dt1 12 -dt2 12 -dt3 423 Riff_1981_05.wav
