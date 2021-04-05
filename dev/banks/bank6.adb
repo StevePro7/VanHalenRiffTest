bc7fcec0359ebd8004b27fcd1dee95bbe277e33b
StevePro7 <steveprozune@hotmail.com>
2021-04-05 22:16:33 +0100

    no message

--- dev/banks/bank6.adb
+++ dev/banks/bank6.adb
@@ -1,2 +1,2 @@
 M:bank6
-S:G$Riff_test_03_wav_pcmenc$0$0({15080}DA15080d,SC:U),D,0,0
+S:G$Riff_test_03_wav_pcmenc$0$0({15297}DA15297d,SC:U),D,0,0
--- dev/banks/bank7.adb
+++ dev/banks/bank7.adb
@@ -1,2 +1,2 @@
 M:bank7
-S:G$Riff_test_04_wav_pcmenc$0$0({13957}DA13957d,SC:U),D,0,0
+S:G$Riff_test_04_wav_pcmenc$0$0({9284}DA9284d,SC:U),D,0,0
